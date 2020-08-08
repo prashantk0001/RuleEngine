5d7f75ffc4e7054ab2c407a5fc88c027780dcf30
Prashant Kashyap <prashantk.0001@gmail.com>
2020-08-08 11:26:08 +0530

    added correct exports

--- index.js
+++ index.js
@@ -389,19 +389,11 @@ const execute = (processMe) => {
     });
 }
 
-const factory = () =>{
-    return {
+module.exports = {
         execute,
         registerRule,
         validOperations
-    }
 }
-
-;(function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-    typeof define === 'function' && define.amd ? define(factory) :
-    global.moment = factory()
-})(this,factory);
 
 
 /* usage */
